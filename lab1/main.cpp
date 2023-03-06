9f6d412254404b7bd4ecaf045c53658741bf4cc6
Michał Fredrych <m.fredrych@o2.pl>
2023-03-06 13:57:51 +0100

    Add initial class structure

---
+++ lab1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+#include <map>
+
+#include "tsp_solver.hpp"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    map<string, TSPSolver> solvers;
+
+    return 0;
+}
