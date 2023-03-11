58fc079185adb02f4d2decc73908bdaef14cb409
mdziecielski <dziecielski.m@gmail.com>
2023-03-11 16:52:09 +0100

    Add helper methods and bool array to track vertices

--- lab1/src/tsp_solver.cpp
+++ lab1/src/tsp_solver.cpp
@@ -18,3 +18,45 @@ void TSPSolver::add_vertex_to_path(int path_no, int vertex_id){
         this->path_length.second += 1;
     }
 }
+
+int TSPSolver::find_furthest_vertex(int vertex_id) {
+    int furthest_vertex_id = -1;
+    int max_distance = -1;
+
+    for (int i = 0; i < N; i++) {
+        if (this->used_vertices[i])
+            continue;
+
+        int distance = this->distance_matrix[vertex_id][i];
+
+        if (distance > max_distance) {
+            furthest_vertex_id = i;
+            max_distance = distance;
+        }
+    }
+
+    this->used_vertices[furthest_vertex_id] = true;
+
+    return furthest_vertex_id;
+}
+
+int TSPSolver::find_nearest_vertex(int vertex_id) {
+    int nearest_vertex_id = -1;
+    int min_distance = INT_MAX;
+
+    for (int i = 0; i < N; i++) {
+        if (this->used_vertices[i])
+            continue;
+
+        int distance = this->distance_matrix[vertex_id][i];
+
+        if (distance < min_distance) {
+            nearest_vertex_id = i;
+            min_distance = distance;
+        }
+    }
+
+    this->used_vertices[nearest_vertex_id] = true;
+
+    return nearest_vertex_id;
+}
--- lab1/src/tsp_solver.h
+++ lab1/src/tsp_solver.h
@@ -13,9 +13,12 @@ public:
     virtual TPaths solve(int start_vertex) = 0;
     void load_data(int [][N]);
     void add_vertex_to_path(int, int);
+    int find_furthest_vertex(int vertex_id);
+    int find_nearest_vertex(int vertex_id);
 
 protected:
     int distance_matrix[N][N];
+    bool used_vertices[N] = { false };
     TPaths paths;
     TPathLength path_length;
 };
