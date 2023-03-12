3bcddc71d6972e775118f8b799dccb2241578a46
Michał Fredrych <m.fredrych@o2.pl>
2023-03-12 22:22:47 +0100

    Add saving test results to CSV

--- lab1/testing/run_tests.py
+++ lab1/testing/run_tests.py
@@ -46,7 +46,11 @@ for filename in os.listdir(DIR_RESULTS):
         "cost_second": [jresults["cost"]["second"]],
         "total_cost": [jresults["cost"]["first"] + jresults["cost"]["second"]]
     })
-
     results = pd.concat([results, results_row], axis=0)
+
+results["start_vertex"] = results["start_vertex"].astype(int)
+results["cost_first"] = results["cost_first"].astype(int)
+results["cost_second"] = results["cost_second"].astype(int)
+results["total_cost"] = results["total_cost"].astype(int)
 
 results.to_csv(os.path.join("test_results.csv"), index=False)
