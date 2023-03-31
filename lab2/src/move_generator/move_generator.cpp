d5471d8733f6cb571833ffb9ca1fe5df307a0149
mdziecielski <dziecielski.m@gmail.com>
2023-04-01 00:12:54 +0200

    Minor refactor

--- lab2/src/move_generator/move_generator.cpp
+++ lab2/src/move_generator/move_generator.cpp
@@ -28,27 +28,27 @@ void MoveGenerator::add_vertex_moves_from_path(TPath path, int path_id, vector<M
     pair<int, int> path_ids(path_id, path_id);
     for (int i = 0; i < path.size() - 1; i++) {
         for (int j = i + 1; j < path.size(); j++) {
-            pair<int, int> vertex_ids(i, j);
-            moves.push_back(new VertexMove(path_ids, vertex_ids));
+            pair<int, int> vertex_idxs(i, j);
+            moves.push_back(new VertexMove(path_ids, vertex_idxs));
         }
     }
 }
 
 void MoveGenerator::add_edge_moves_from_path(TPath path, int path_id, vector<Move*>& moves) {
     for (int i = 0; i < path.size() - 1; i++) {
-        TEdge edge1_ids(i, i + 1);
+        TEdge edge1_idxs(i, i + 1);
         
         for (int j = i; j < path.size(); j++) {
-            TEdge edge2_ids;
+            TEdge edge2_idxs;
 
             if (j == (path.size() - 1))
-                edge2_ids = make_pair(j, 0);
+                edge2_idxs = make_pair(j, 0);
             else
-                edge2_ids = make_pair(j, j + 1);
+                edge2_idxs = make_pair(j, j + 1);
 
-            TEdges edge_ids(edge1_ids, edge2_ids);
+            TEdges edge_idxs(edge1_idxs, edge2_idxs);
 
-            moves.push_back(new EdgeMove(path_id, edge_ids));
+            moves.push_back(new EdgeMove(path_id, edge_idxs));
         }
     }
 }
@@ -57,8 +57,8 @@ void MoveGenerator::add_vertex_moves_from_paths(TPaths paths, vector<Move*>& mov
     pair<int, int> path_ids(0, 1);
     for (int i = 0; i < paths.first.size(); i++) {
         for (int j = 0; j < paths.second.size(); j++) {
-            pair<int, int> vertex_ids(i, j);
-            moves.push_back(new VertexMove(path_ids, vertex_ids));
+            pair<int, int> vertex_idxs(i, j);
+            moves.push_back(new VertexMove(path_ids, vertex_idxs));
         }
     }
 }
