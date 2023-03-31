6324f8fbf815bd54cfdbbd8f202e330b5036b4d3
Michał Fredrych <m.fredrych@o2.pl>
2023-03-31 21:16:37 +0200

    Remove pure abstract functions.

--- lab2/src/tsp_solver.h
+++ lab2/src/tsp_solver.h
@@ -8,8 +8,8 @@ using namespace std;
 class TSPSolver
 {
 public:
-    virtual TPaths solve(int start_vertex) = 0;
-    virtual TPaths solve(TPaths start_solution) = 0;
+    virtual TPaths solve(int start_vertex) {return this->paths;}
+    virtual TPaths solve(TPaths start_solution) {return this->paths;}
     
     void load_data(int [][N]);
     void add_vertex_to_path(int path_no, int vertex_id, int index = -1);
