916e981d031c6a1dfcf58d007b5fa99212e07ca5
mdziecielski <dziecielski.m@gmail.com>
2023-04-01 14:10:46 +0200

    Implement EdgeMove

--- lab2/src/moves/edge_move.cpp
+++ lab2/src/moves/edge_move.cpp
@@ -1,4 +1,5 @@
 #include "edge_move.h"
+#include <cmath>
 
 EdgeMove::EdgeMove(int path_id, TEdges edge_idxs) {
     this->path_id = path_id;
@@ -6,16 +7,37 @@ EdgeMove::EdgeMove(int path_id, TEdges edge_idxs) {
 }
 
 void EdgeMove::apply(TPaths &paths) {
-    // TODO
+    int v11 = this->edge_idxs.first.first, v12 = this->edge_idxs.first.second;
+    int v21 = this->edge_idxs.second.first, v22 = this->edge_idxs.second.second;
+
+    int n = round((float)(v21 - v12) / 2.0);
+
+    if (this->path_id == 0)
+        for (int i = 0; i < n; i++)
+            swap(paths.first[v12 + i], paths.first[v21 - i]);
+
+    if (this->path_id == 1)
+        for (int i = 0; i < n; i++)
+            swap(paths.second[v12 + i], paths.second[v21 - i]);
 }
 
 void EdgeMove::print() {
-    printf("e11 = %d, e12 = %d, e21 = %d, e22 = %d, p = %d\n", this->edge_idxs.first.first, this->edge_idxs.first.second, this->edge_idxs.second.first, this->edge_idxs.second.second, this->path_id);
+    printf("v11 = %d, v12 = %d, v21 = %d, v22 = %d, p = %d\n", this->edge_idxs.first.first, this->edge_idxs.first.second, this->edge_idxs.second.first, this->edge_idxs.second.second, this->path_id);
 }
 
 TPathCost EdgeMove::get_cost_delta(TPaths paths, int distance_matrix[][N]) {
-    // TODO
-    TPathCost costs = make_pair(0, 0);
+    int v11 = this->edge_idxs.first.first, v12 = this->edge_idxs.first.second;
+    int v21 = this->edge_idxs.second.first, v22 = this->edge_idxs.second.second;
 
-    return costs;
+    int current_cost = distance_matrix[v11][v12] + distance_matrix[v21][v22];
+    int new_cost = distance_matrix[v11][v21] + distance_matrix[v12][v22];
+    
+    TPathCost delta_cost(0, 0);
+
+    if (this->path_id == 0)
+        delta_cost.first = new_cost - current_cost;
+    else
+        delta_cost.second = new_cost - current_cost;
+
+    return delta_cost;
 }
