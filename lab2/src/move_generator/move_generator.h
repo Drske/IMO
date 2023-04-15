3aa07a7affd5c19249f96a750aebafdcd2e34b28
mdziecielski <dziecielski.m@gmail.com>
2023-04-15 21:27:27 +0200

    Implement generating new moves

--- lab2/src/move_generator/move_generator.h
+++ lab2/src/move_generator/move_generator.h
@@ -3,16 +3,24 @@
 
 #include "../types.h"
 #include "../moves/move.h"
+#include "../moves/edge_move.h"
+#include "../moves/vertex_move.h"
 
 class MoveGenerator
 {
     public:
         static vector<Move*> get_first_neighbourhood_moves(TPaths paths);
         static vector<Move*> get_second_neighbourhood_moves(TPaths paths);
+        static vector<Move*> get_new_moves_after_move(TPaths paths, VertexMove* vertexMove);
+        static vector<Move*> get_new_moves_after_move(TPaths paths, EdgeMove* edgeMove);
     private:
         static void add_vertex_moves_from_path(TPath path, int path_id, vector<Move*>& moves);
         static void add_edge_moves_from_path(TPath path, int path_id, vector<Move*>& moves);
         static void add_vertex_moves_from_paths(TPaths paths, vector<Move*>& moves);
+
+        static void add_vertex_moves_after_move(TPath path, int path_id, vector<int> path_modified_idxs, vector<Move*>& moves);
+        static void add_vertex_moves_after_move(TPaths paths, pair<vector<int>, vector<int>> paths_modified_idxs, vector<Move*>& moves);
+        static void add_edge_moves_after_move(TPath path, int path_id, vector<int> path_modified_idxs, vector<Move*>& moves);
 };
 
 #endif
