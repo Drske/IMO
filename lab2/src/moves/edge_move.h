cc9ce2f74e5da24105a57f3e9ce2db4592549e48
mdziecielski <dziecielski.m@gmail.com>
2023-04-16 16:24:44 +0200

    Small fix

--- lab2/src/moves/edge_move.h
+++ lab2/src/moves/edge_move.h
@@ -14,6 +14,7 @@ public:
     TPathCost get_cost_delta(TPaths paths, int distance_matrix[][N]);
     MoveState checkMoveState(TPaths paths);
     TEdges get_edge_idxs();
+    TEdges get_edge_ids();
     int get_path_id();
 private:
     int path_id;
